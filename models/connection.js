df05113d01d412cb33e09d86d6499af6cabf699a
Faty <fatoudialloo95@gmail.com>
2024-04-16 14:41:42 +0200

    BDD ok

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://fatoudiallooo:Orange24iLoL@cluster0.kbtyib6.mongodb.net/Tickethack';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
